54f87b1328d2cb29a6c3bfece56133c398e2e50b
Gorka <gorkavillara@gmail.com>
2022-06-16 12:38:09 +0200

    Añadida máxima puntuación en inicio

--- src/components/routes/MainRoute.vue
+++ src/components/routes/MainRoute.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pa6">
+  <div class="pa6 main-route">
     <h1 class="title1 text-left text-bold mb6">Palabras</h1>
     <w-flex column class="align-center" gap="3">
       <button
@@ -20,10 +20,61 @@
         </w-flex>
       </button>
     </w-flex>
+    <div class="record">
+      <div v-if="maxPoints > 0" class="active">
+        <span class="points">
+          <w-icon class="mr2">fa fa-certificate</w-icon><span>Máxima puntuación:
+          {{ maxPoints }}</span>
+        </span>
+        <span class="games">En un total de {{ tries.length }} partidas</span>
+      </div>
+      <div v-else class="inactive">
+        <span class="points"> Todavía no tienes ningún récord </span>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<script lang="ts">
+import { onMounted, ref } from "@vue/runtime-core";
+
+type Try = {
+  id: number;
+  word: string;
+  correct: boolean;
+  points: number;
+};
+
+type StoredTry = {
+  points: number;
+  letters: string[];
+  tries: Try[];
+  date: Date;
+};
+
+export default {
+  setup() {
+    const tries = ref<StoredTry[]>([]);
+    const maxPoints = ref<number>(0);
+    onMounted(() => {
+      const storedTries = localStorage.getItem("wordGameTries");
+      tries.value = storedTries !== null ? JSON.parse(storedTries) : [];
+      maxPoints.value = tries.value
+        .map((tr: StoredTry) => tr.points)
+        .reduce((a, b) => (b > a ? b : a), 0);
+    });
+    return {
+      tries,
+      maxPoints,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.main-route {
+  height: 100%;
+}
 .option-card-blue {
   width: 90%;
   height: 150px;
@@ -36,5 +87,31 @@
   border-radius: 1.5rem;
   outline: none;
   border: none;
+}
+.record {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 80%;
+  bottom: 10px;
+  border-radius: 1.5rem;
+  background: rgb(188, 191, 0);
+  background: linear-gradient(0deg, rgb(191, 146, 0) 0%, rgb(255, 187, 0) 100%);
+  padding: 1rem;
+  color: rgb(255, 255, 255);
+  .active {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .points {
+      font-size: 20px;
+      display: flex;
+      align-items: center;
+    }
+    .games {
+      font-size: 12px;
+      font-style: italic;
+    }
+  }
 }
 </style>
